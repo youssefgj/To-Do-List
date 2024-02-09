347548b4346e1a4fff70184f1d3a18e27c248c79
Youssef Graja <youssefgraja1965@gmail.com>
2024-02-09 14:08:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+function AddTask(){
+    if(inputBox.value===''){
+        alert("you need to write something!")
+    }
+    else{
+      let  li=document.createElement("li");
+      li.innerHTML=inputBox.value;
+      listContainer.appendChild(li);
+      let span=document.createElement("span")
+      span.innerHTML="\u00d7" ;
+      li.appendChild(span);
+    }
+    inputBox.value=""
+
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI") e.target.classList.toggle("checked");
+    else if(e.target.tagName==="SPAN") e.target.parentElement.remove();
+},false);
